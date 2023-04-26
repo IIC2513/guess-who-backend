45b1741be28dddcd6f74032bae98adacdc060284
ElTioAndresCabezas <ascabezas@uc.cl>
2023-04-26 12:57:59 -0400

    feat: Servidor sencillo de Koa con Hola Mundo

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import Koa from "koa";
+import KoaLogger from "koa-logger";
+import { koaBody } from "koa-body";
+
+// Crear instancia de Koa
+const app = new Koa();
+
+// Middlewares proporcionados por Koa
+app.use(KoaLogger());
+app.use(koaBody());
+
+// Middleware personalizado. Encargado de dar respuesta "Hola Mundo!"
+app.use((ctx, next) => {
+  ctx.body = "Hola Mundo! Saludos desde IIC2513";
+});
+
+// Hacer que el servidor escuche en el puerto 3000
+app.listen(3000, () => {
+  console.log("Iniciando app. Escuchando en el puerto 3000")
+});
