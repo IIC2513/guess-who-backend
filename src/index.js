03212866c8e85f9e66783984a337fbe83cb83a2a
Fernando Smith <pini@pop-os.localdomain>
2023-05-04 15:44:09 -0400

    f: add routes middleware

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Koa from "koa";
 import KoaLogger from "koa-logger";
 import { koaBody } from "koa-body";
+import router from "./routes.js";
+
 
 // Crear instancia de Koa
 const app = new Koa();
@@ -8,6 +10,9 @@ const app = new Koa();
 // Middlewares proporcionados por Koa
 app.use(KoaLogger());
 app.use(koaBody());
+
+// koa router
+app.use(router.routes());
 
 // Middleware personalizado. Encargado de dar respuesta "Hola Mundo!"
 app.use((ctx, next) => {
