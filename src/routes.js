f67a7a922d700aaf4b9de57258114d9779a1b2af
Camila González <ccgonzalez3@uc.cl>
2023-05-11 23:06:00 -0400

    Add Sequelize config

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-import Router from "koa-router";
-import characters from "./routes/characters.js";
+const Router = require('koa-router');
+const characters = require('./routes/characters.js');
 
 const router = new Router();
 
 router.use('/characters', characters.routes());
 
-export default router;
+module.exports = router;
