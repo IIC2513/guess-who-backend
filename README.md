0862650080b906fa165ae22283e222860a06fd46
Camila González <ccgonzalez3@uc.cl>
2023-05-13 17:46:42 -0400

    Update folders

--- README.md
+++
@@ -1,1 +0,0 @@
-# guess-who-backend
--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router');
-const characters = require('./routes/characters.js');
-
-const router = new Router();
-
-router.use('/characters', characters.routes());
-
-module.exports = router;
