97ccc644d7844b85dd36b6fa2c74cf64068d32c0
Fran <faibarra1@uc.cl>
2023-06-03 00:44:14 -0400

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const KoaLogger = require('koa-logger');
 const { koaBody } = require('koa-body');
+const cors = require('@koa/cors');
 const router = require('./routes.js');
 const orm = require('./models');
 
@@ -9,6 +10,9 @@ const app = new Koa();
 
 // Exponer el orm a la app
 app.context.orm = orm;
+
+// Cors para poder acceder desde el frontend
+app.use(cors());
 
 // Middlewares proporcionados por Koa
 app.use(KoaLogger());
