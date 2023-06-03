90631122d75e140ee11a1134e28f41084adc9718
Fran <faibarra1@uc.cl>
2023-06-03 11:43:41 -0400

    fix: remove unused endpoint on board routes

--- src/routes/boards.js
+++ src/routes/boards.js
@@ -23,21 +23,4 @@ router.get("board.getBoardCharacters","/boardData",async(ctx)=>{
     }
 })
 
-router.put("board.toggleCharacter", "/toggle/:board_id/:character_id", async(ctx) => {
-    try{
-        // De la request viene el id del personaje, el id del tablero. 
-        // Con esto podemos modificar la base de datos para cambiar el status 
-        // "oculto" del personaje seleccionado.
-        const character = await ctx.orm.Character.findOne({where:{id:ctx.params.character_id}});
-        const result = await character.update(
-            { oculto: !character.oculto },
-        )
-        ctx.body = result;
-        ctx.status = 200;
-    } catch(error){
-        ctx.body = error;
-        ctx.status = 400;
-    }
-})
-
 module.exports = router;
